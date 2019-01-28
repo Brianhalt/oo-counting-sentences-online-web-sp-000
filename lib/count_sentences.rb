4e1def5bd86d0d400fb87f980ff69f6ea284e180
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-28 15:12:39 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,7 +28,7 @@ class String
 
   def count_sentences
       new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-      binding.pry
+      # binding.pry
       return new_array.length
     end
 end
